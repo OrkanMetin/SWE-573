c015ee5364227680e37f9bdd02aa3fdc41db2af5
orkanmetin <orkanmetin@gmail.com>
2019-11-18 18:34:40 +0300

    Model.PY has been updated: User Community FieldType PostType Post PostDetails (it will store other fields of the Post in JSON)

--- community/models.py
+++ community/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib import auth
 from django.contrib.auth import get_user_model
+from django.contrib.postgres.fields import JSONField
 
 
 class User(models.Model):
@@ -14,8 +15,8 @@ class User(models.Model):
     userSurname = models.CharField(max_length=50, null=True, help_text='Enter your surname')
     userMail = models.EmailField()
     userPassword = models.CharField(max_length=50, null=True, help_text='Enter your password')
-    creationDate = models.DateTimeField(null=True)
-    userPhoto = models.ImageField(upload_to='users')
+    creationDate = models.DateTimeField(default=timezone.now)
+    userPhoto = models.ImageField(upload_to='users', blank=True)
     isActive = models.BooleanField(null=True)
 
     def __str__(self):
@@ -28,29 +29,37 @@ class Community(models.Model):
     """
     communityName = models.CharField(max_length=100, null=True, help_text='Enter community name')
     communityDesc = models.CharField(max_length=200, null=True, help_text='Enter community description')
+    communitySemanticTag = models.CharField(max_length=100, null=True, help_text='Enter related semantic tag')
     communityPhoto = models.ImageField(upload_to='communities')
     communityOwner = models.ForeignKey(User, related_name='owners', on_delete=models.SET_NULL, null=True)
     communityMembers = models.ManyToManyField(User, related_name='members', help_text='Community members')
-    communityCreationDate = models.DateTimeField(null=True)
+    communityCreationDate = models.DateTimeField(default=timezone.now)
     isActive = models.BooleanField(null=True)
 
     def __str__(self):
         return self.communityName
 
 
+class FieldType(models.Model):
+    name = models.CharField(max_length=100, null=True, help_text='Enter data type name')
+    # CharField eg: Geolocation
+    maxLength = models.CharField(max_length=100, null=True, help_text='Maxlength')
+    # eg: maxlength=100
+    isNull = models.CharField(max_length=100, null=True, help_text='Can be null?')
+
+    # eg: maxlength=100
+
+    def __str__(self):
+        return self.name
+
+
 class PostType(models.Model):
     postTypeName = models.CharField(max_length=100, null=True, help_text='Enter post type name')
+    postTypeDesc = models.CharField(max_length=1200, null=True, help_text='Enter post type description')
     isActive = models.BooleanField(null=True)
 
     def __str__(self):
         return self.postTypeName
-
-
-class DataType(models.Model):
-    dataTypeName = models.CharField(max_length=100, null=True, help_text='Enter post type name')
-
-    def __str__(self):
-        return self.dataTypeName
 
 
 class Post(models.Model):
@@ -64,7 +73,14 @@ class Post(models.Model):
     postCreationDate = models.DateTimeField(null=True)
 
     def __str__(self):
-        return self.propertyValue
+        return self.postTitle
 
 
+class PostDetails(models.Model):
+    relatedPost = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True)
+    data = JSONField()
 
+    # JSON field will have all the data fields defined by Community Owner.
+
+    def __str__(self):
+        return self.data
