c21a47655ecf31c3da4ca4ae6db6d9fa96671566
orkanmetin <orkanmetin@gmail.com>
2019-11-19 09:03:49 +0300

    All the property names have been updated. The naming rule is that I use lowercase and underscores for properties.

--- community/models.py
+++ community/models.py
@@ -10,46 +10,46 @@ class User(models.Model):
     """
     Model whose primary purpose is to display user's profile data.
     """
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='user')
-    userName = models.CharField(max_length=50, null=True, help_text='Enter your name')
-    userSurname = models.CharField(max_length=50, null=True, help_text='Enter your surname')
-    userMail = models.EmailField()
-    userPassword = models.CharField(max_length=50, null=True, help_text='Enter your password')
-    creationDate = models.DateTimeField(default=timezone.now)
-    userPhoto = models.ImageField(upload_to='users', blank=True)
-    isActive = models.BooleanField(null=True)
+    user_name = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='user')
+    first_name = models.CharField(max_length=50, null=True, help_text='Enter your name')
+    surname = models.CharField(max_length=50, null=True, help_text='Enter your surname')
+    email = models.EmailField()
+    password = models.CharField(max_length=50, null=True, help_text='Enter your password')
+    created_date = models.DateTimeField(default=timezone.now)
+    user_photo = models.ImageField(upload_to='users', blank=True)
+    is_active = models.BooleanField(null=True)
 
     def __str__(self):
-        return self.user
+        return self.user_name
 
 
 class Community(models.Model):
     """
     Model whose primary purpose is to display Community's data.
     """
-    communityName = models.CharField(max_length=100, null=True, help_text='Enter community name')
-    communityDesc = models.CharField(max_length=200, null=True, help_text='Enter community description')
-    communitySemanticTag = models.CharField(max_length=100, null=True, help_text='Enter related semantic tag')
-    communityPhoto = models.ImageField(upload_to='communities')
-    communityOwner = models.ForeignKey(User, related_name='owners', on_delete=models.SET_NULL, null=True)
-    communityMembers = models.ManyToManyField(User, related_name='members', help_text='Community members')
-    communityCreationDate = models.DateTimeField(default=timezone.now)
-    isActive = models.BooleanField(null=True)
+    name = models.CharField(max_length=100, null=True, help_text='Enter community name')
+    desc = models.CharField(max_length=200, null=True, help_text='Enter community description')
+    semantic_tag = models.CharField(max_length=100, null=True, help_text='Enter related semantic tag')
+    community_photo = models.ImageField(upload_to='communities')
+    owner = models.ForeignKey(User, related_name='owners', on_delete=models.SET_NULL, null=True)
+    members = models.ManyToManyField(User, related_name='members', help_text='Community members')
+    created_date = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(null=True)
 
     def publish(self):
         self.communityCreationDate = timezone.now()
         self.save()
 
     def __str__(self):
-        return self.communityName
+        return self.name
 
 
 class FieldType(models.Model):
     name = models.CharField(max_length=100, null=True, help_text='Enter data type name')
     # CharField eg: Geolocation
-    maxLength = models.CharField(max_length=100, null=True, help_text='Maxlength')
+    max_length = models.CharField(max_length=100, null=True, help_text='Maxlength')
     # eg: maxlength=100
-    isNull = models.CharField(max_length=100, null=True, help_text='Can be null?')
+    is_null = models.CharField(max_length=100, null=True, help_text='Can be null?')
 
     # eg: maxlength=100
 
@@ -58,39 +58,39 @@ class FieldType(models.Model):
 
 
 class PostType(models.Model):
-    postTypeName = models.CharField(max_length=100, null=True, help_text='Enter post type name')
-    postTypeDesc = models.CharField(max_length=1200, null=True, help_text='Enter post type description')
-    postTypeCreatedDate = communityCreationDate = models.DateTimeField(default=timezone.now)
-    isActive = models.BooleanField(null=True)
+    name = models.CharField(max_length=100, null=True, help_text='Enter post type name')
+    desc = models.CharField(max_length=1200, null=True, help_text='Enter post type description')
+    created_date = communityCreationDate = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(null=True)
 
     def publish(self):
-        self.postTypeCreatedDate = timezone.now()
+        self.created_date = timezone.now()
         self.save()
 
     def __str__(self):
-        return self.postTypeName
+        return self.name
 
 
 class Post(models.Model):
-    relatedCommunity = models.ForeignKey(Community, on_delete=models.SET_NULL, null=True)
-    relatedPostType = models.ForeignKey(PostType, on_delete=models.SET_NULL, null=True)
-    postTitle = models.CharField(max_length=100, null=True, help_text='Enter title of post as text')
-    postBody = models.CharField(max_length=200, null=True, help_text='Enter body of post as text')
-    postTag = models.CharField(max_length=100, null=True, help_text='Tag comes from related community tag')
-    isActive = models.BooleanField(null=True)
-    postCreator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    postCreationDate = models.DateTimeField(null=True)
+    related_community = models.ForeignKey(Community, on_delete=models.SET_NULL, null=True)
+    related_post_type = models.ForeignKey(PostType, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=100, null=True, help_text='Enter title of post as text')
+    body = models.CharField(max_length=200, null=True, help_text='Enter body of post as text')
+    semantic_tag = models.CharField(max_length=100, null=True, help_text='Tag comes from related community tag')
+    is_active = models.BooleanField(null=True)
+    creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    created_date = models.DateTimeField(null=True)
 
     def publish(self):
-        self.postCreationDate = timezone.now()
+        self.created_date = timezone.now()
         self.save()
 
     def __str__(self):
-        return self.postTitle
+        return self.title
 
 
 class PostDetails(models.Model):
-    relatedPost = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True)
+    related_post = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True)
     data = JSONField()
 
     # JSON field will have all the data fields defined by Community Owner.
