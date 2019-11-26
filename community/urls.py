f51de4551badc27b651b2ba3d8f3726c7b813b45
orkanmetin <orkanmetin@gmail.com>
2019-11-26 16:31:16 +0300

    edit_post and new_post forms are added. Security issue is in progress: {% if user.is_authenticated %}

--- community/urls.py
+++ community/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('post_list', views.post_list, name='post_list'),
     path('community/<int:pk>/', views.community_detail, name='community_detail'),
     path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- community/views.py
+++ community/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from django.shortcuts import redirect
+from .forms import PostForm
 from .models import Community, Post
 
 
@@ -25,3 +27,32 @@ def post_list(request):
 def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
     return render(request, 'community/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.creator = request.user
+            post.publish_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'community/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.creator = request.user
+            post.publish_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'community/post_edit.html', {'form': form})
