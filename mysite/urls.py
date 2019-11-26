db4c4272d3eea1f7f91d4e6ae0c8f2602832e6cb
orkanmetin <orkanmetin@gmail.com>
2019-11-26 18:20:25 +0300

    Milestone 3 is completed 100 %.

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
+from django.contrib.auth import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('community.urls'))
 ]
