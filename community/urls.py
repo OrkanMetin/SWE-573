46ab41fd922cff85529a0b329fa7b9f84722de74
orkanmetin <orkanmetin@gmail.com>
2019-11-26 14:33:15 +0300

    post_detail and post_list templates are added. Header is updated.

--- community/urls.py
+++ community/urls.py
@@ -5,8 +5,13 @@ from django.contrib.staticfiles.urls import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
+    path('post_list', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('', views.community_list, name='community_list'),
+    path('community_list', views.community_list, name='community_list'),
+    path('post_list', views.post_list, name='post_list'),
     path('community/<int:pk>/', views.community_detail, name='community_detail'),
+    path('post/new/', views.post_new, name='post_new'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- community/views.py
+++ community/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from .models import Community
+from .models import Community, Post
+
+
 # Create your views here.
 
 
 def community_list(request):
-    communities = Community.objects.order_by('name')
-    return render(request, 'community/community_list.html', {'communities': communities})
+    community = Community.objects.order_by('name')
+    return render(request, 'community/community_list.html', {'community': community})
 
 
 def community_detail(request, pk):
     community = Community.objects.get(pk=pk)
     return render(request, 'community/community_detail.html', {'community': community})
+
+
+def post_list(request):
+    posts = Post.objects.order_by('title')
+    return render(request, 'community/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'community/post_detail.html', {'post': post})
